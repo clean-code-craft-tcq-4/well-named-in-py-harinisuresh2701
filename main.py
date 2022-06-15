e550564f9f7d2d0b857359c1d39c0d88b25b2b3e
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-06-15 15:27:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-def color_pair_to_string(major_color, minor_color):
-  return f'{major_color} {minor_color}'
-  
+import ConvertColorToPairNumber
+import ConvertPairNumberToColor
+import TestColorCodeConversion
 if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
+  MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+  MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+  for i in MAJOR_COLORS:
+    for j in MINOR_COLORS:
+      pair_num = ConvertColorToPairNumber.get_pair_number_from_color(i,j)
+      print("Color Code Reference Manual: \n")
+      print(pair_num,"\t",i,"\t",j,"\n")
+  TestColorCodeConversion.test_number_to_pair(4, 'White', 'Brown')
+  TestColorCodeConversion.test_number_to_pair(5, 'White', 'Slate')
+  TestColorCodeConversion.test_pair_to_number('Black', 'Orange', 12)
+  TestColorCodeConversion.test_pair_to_number('Violet', 'Slate', 25)
+  TestColorCodeConversion.test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
